654c982477b23051af693fe46482adbd8eaa1f25
alexxx16386 <38440891+alexxx16386@users.noreply.github.com>
2020-04-27 17:44:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['postgres://lbnvhfjrssyoxp:adee998fb724fd6fb676d478eb1e1a309cac276a57ac8fe476a192c8f81d90e2@ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/dfj6k2nuk6jpn3']
+DATABASE_URL = os.environ['postgres://scqsmpycssazcb:c5ebcd79218efc56684550121367ab8f04235b54b5fcdeb23fd4cf25c346de96@ec2-54-247-103-43.eu-west-1.compute.amazonaws.com:5432/ddvgha19k56bn2']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
